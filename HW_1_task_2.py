e1f9c269acecb3cd6b5ae73d239e24c5b587c932
DariSarfaty <daris@mail.tau.ac.il>
2024-06-06 18:35:19 +0300

    did some cool stuff

--- HW_1_task_2.py
+++ HW_1_task_2.py
@@ -16,39 +16,17 @@ def f(coor):
     y = coor[1]
     return 4 * y * y + 4 * y - 52 * x - 19
 
-def fy(coor):
-    x = coor[0]
-    y = coor[1]
-    return 8  * y + 4
-
-def fx(coor):
-    x = coor[0]
-    y = coor[1]
-    return - 52
 
 def g(coor):
     x = coor[0]
     y = coor[1]
     return 169 * x * x + 3 * y * y - 111 * x - 10 * y
 
-def gy(coor):
-    x = coor[0]
-    y = coor[1]
-    return 6 * y - 10
-
-def gx(coor):
-    x = coor[0]
-    y = coor[1]
-    return 169 * 2 * x - 111
-
-
-
 
 print(f"found the following solution: {Tools.newton(f, g, [-0.01, -0.01], 0.00000001)}")
-"""for some reason epsilon isn't factored in well"""
 
 
-
+"""c. plot the requested function"""
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
 # Make data.
